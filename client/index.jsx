573ec5bbd5f809d7958b173e7e1345fdae982415
Daxo <17711952+Daxo@users.noreply.github.com>
2019-07-15 14:21:49 -0500

    Changed Font to Amazon Ember

--- client/index.jsx
+++ client/index.jsx
@@ -2,8 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import configureStore from './store/configureStore';
+import './styles/scss/index.scss';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
+// eslint-disable-next-line import/extensions
 import App from './components/App.jsx';
 
 const store = configureStore();
