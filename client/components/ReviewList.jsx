a1345e8adafdf8812229e3e4b4d6ed1c066ee4b6
Daxo <17711952+Daxo@users.noreply.github.com>
2019-07-15 20:09:42 -0500

    Renders a list of Review components and displays User, Summary, and ReviewText.

--- client/components/ReviewList.jsx
+++ client/components/ReviewList.jsx
@@ -1,7 +1,6 @@
 /* eslint-disable react/prefer-stateless-function */
 /* eslint-disable react/prop-types */
 import React from 'react';
-import { uuid4 } from 'uuid/v4';
 import { connect } from 'react-redux';
 import Review from './Review.jsx';
 
--- client/components/ReviewSummary.jsx
+++ client/components/ReviewSummary.jsx
@@ -1,7 +1,14 @@
 import React from 'react';
 import '../styles/scss/ReviewSummary.scss';
+import PropTypes from 'prop-types';
+
 function ReviewSummary(props) {
-  return <div className="bold">Review Summary</div>;
+  const { summary } = props;
+  return <div className="bold">{summary}</div>;
 }
+
+ReviewSummary.propTypes = {
+  summary: PropTypes.string.isRequired
+};
 
 export default ReviewSummary;
