c4ddb0a2046c0c695976769d6b4c9d486b095b27
Garrett Welson <garrettwelson@gmail.com>
2019-07-24 11:22:21 -0500

    Test of Travis CI setup

---
+++ fakerTest.js
@@ -0,0 +1,13 @@
+const faker = require('faker');
+
+faker.seed(123);
+
+let products = [];
+
+for (let i = 0; i < 10000000; i++) {
+  products.push(faker.commerce.productName())
+}
+
+console.log(products.length);
+
+//testing Travis CI
