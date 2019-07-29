8854e154d820b19bb9b22806b06782d35ebab278
Garrett Welson <garrettwelson@gmail.com>
2019-07-29 17:47:04 -0500

    Make CSV file with 0-5 reviews for 10M products

---
+++ db_seeding/csvWriter.js
@@ -0,0 +1,62 @@
+const faker = require('faker');
+// const createCsvWriter = require('csv-writer').createObjectCsvWriter;
+const csvWriter = require('csv-write-stream')
+const writer = csvWriter({sendHeaders: true, headers: ["asin", "reviewerID", "reviewerName", "helpful", "text", "overall", "summary", "UNIX", "time"]})
+const fs = require('fs');
+
+/*
+      "_id": "5d20f7c9100f12f17dee37df",
+      "reviewerID": "A3AYF9CD4PXDJR",
+      "asin": "B01D1NMLJU",
+      "reviewerName": "E. Orchard \"I love lamp\"",
+      "helpful": [
+          11,
+          13
+      ],
+      "reviewText": "You can't beat the price for this kit and it's so easy to start seeds. I used this for the first time last year and followed planting and disk instructions and had over a 75% germination rate! Between the cost and ease-of-use, this is a great way to start seeds for outdoors. Just keep the little pellets moist and you'll be good to go.Some seeds need dark to germinate, so make sure to read your seed packets!",
+      "overall": 5,
+      "summary": "Great success!",
+      "unixReviewTime": 1262044800,
+      "reviewTime": "12 29, 2009"
+  }
+*/
+
+// const csvWriter = createCsvWriter({
+//   path: './db_seeding/reviewData.csv',
+//   header: [
+//       {id: 'asin', title: 'ASIN'},
+//       {id: 'reviewerID', title: 'REVIEWER_ID'},
+//       {id: 'reviewerName', title: 'REVIEWER_NAME'},
+//       {id: 'helpful', title: 'HELPFUL'},
+//       {id: 'text', title: 'REVIEW_TEXT'},
+//       {id: 'overall', title: 'OVERALL'},
+//       {id: 'summary', title: 'REVIEW_SUMMARY'},
+//       {id: 'UNIX', title: 'UNIX_TIME'},
+//       {id: 'time', title: 'REVIEW_TIME'},
+//   ]
+// });
+
+//const data = [];
+writer.pipe(fs.createWriteStream('./db_seeding/reviewData.csv',{flags: 'a'}));
+
+for (let i = 9000001; i < 10000000; i++) {
+  reviewCount = faker.random.number(5)
+  for (let j = 0; j < reviewCount; j++) {
+    writer.write({
+      asin: i,
+      reviewerID: faker.random.alphaNumeric(8),
+      reviewerName: faker.name.findName(),
+      helpful: `['${faker.random.number(99)}','${faker.random.number(10)}']`,
+      text: `My ${faker.commerce.productName()} was ${faker.commerce.productAdjective()} and made of ${faker.commerce.productMaterial()}`,
+      overall: faker.random.number(5),
+      summary: faker.random.words(4),
+      UNIX: faker.random.number({max:1564418741}),
+      time: faker.helpers.replaceSymbolWithNumber('## ##, ####')
+    })
+  }
+}
+
+writer.end()
+
+//csvWriter.writeRecords(data)
+
